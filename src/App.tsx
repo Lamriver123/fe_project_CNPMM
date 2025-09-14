3baa4b21d31be6cd4810f5c77de0aa070b963339
DatHocVietCode <td13052004@gmail.com>
2025-09-14 23:10:18 +0700

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Body from "./components/layout/Body.tsx";
 import Footer from "./components/layout/Footer.tsx";
@@ -6,24 +5,17 @@ import Header from "./components/layout/Header.tsx";
 import Navbar from "./components/layout/Navbar.tsx";
 import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage.tsx";
 import HomePage from "./pages/auth/HomePage.tsx";
-=======
 import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
->>>>>>> 0c8b5585ab47bf03fafcbd47f767e35d438dfb0b
 import LoginPage from "./pages/auth/LoginPage.tsx";
 import RegisterPage from "./pages/auth/RegisterPage.tsx";
 import VerifyOtpPage from "./pages/auth/VerifyOtpPage.tsx";
 import CategoryPage from "./pages/products/CategoryPage.tsx";
 import ProductDetailPage from "./pages/products/ProductDetailPage.tsx";
-<<<<<<< HEAD
 import ProductsPage from "./pages/products/ProductsPage.tsx";
-=======
 import ProfilePage from "./pages/profile/ProfilePage.tsx";
 import { setToken, updateUser } from "./redux/authSlice.ts";
 import { useDispatch } from "react-redux";
 import { profileApi } from "./api/profileApi.ts";
->>>>>>> 0c8b5585ab47bf03fafcbd47f767e35d438dfb0b
-
 export default function App() {
   const dispatch = useDispatch();
 
@@ -66,11 +58,10 @@ export default function App() {
           <Route path="/verify-otp" element={<VerifyOtpPage />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/products/:id" element={<ProductDetailPage />} />
-<<<<<<< HEAD
+
           <Route path="/category/:category" element={<CategoryPage />} />
-=======
           <Route path="/profile" element={<ProfilePage />} />
->>>>>>> 0c8b5585ab47bf03fafcbd47f767e35d438dfb0b
+
         </Routes>
       </Body>
       <Footer />
