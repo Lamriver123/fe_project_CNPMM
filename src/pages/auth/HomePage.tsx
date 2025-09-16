9b7493b81085ddf1fc4901f1f4d81009eb262817
DatHocVietCode <td13052004@gmail.com>
2025-09-16 20:06:06 +0700

    fix load products in HomePage.tsx

--- src/pages/auth/HomePage.tsx
+++ src/pages/auth/HomePage.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ProductsSection from "../../components/products/ProductsSection.tsx";
 import { Product } from "../../types/Product.ts";
 import "./HomePage.css";
-
+import { productApi } from "../../api/productApi.ts";
 
 // interface Product {
 //   id: number;
@@ -17,116 +17,116 @@ import "./HomePage.css";
 // }
 
 
-const products: Product[] = [
-{
-    id: 1,
-    name: "Balo UTE Premium",
-    price: 299000,
-    originalPrice: 399000,
-    images: 
-    [
-      "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=400&h=400&fit=crop",
-      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400&h=400&fit=crop",
-      "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?w=400&h=400&fit=crop"
-    ],
-    // image: "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=400&h=400&fit=crop",
-    category: "Balo",
-    isHot: true,
-    createdAt: Date.now(),
-    sold: 120,
-    views: 500,
-    discountPercent: 25,
-    stock: 20
-  },
-  {
-    id: 2,
-    name: "Áo Polo UTE Classic",
-    price: 189000,
-    originalPrice: 249000,
-    images: ["https://images.unsplash.com/photo-1581655353564-df123a1eb820?w=400&h=400&fit=crop"],
-    category: "Áo",
-    isNew: true,
-    createdAt: Date.now(),
-    sold: 300,
-    views: 800,
-    discountPercent: 20,
-     stock: 20,
-  },
-  {
-    id: 3,
-    name: "Nón Snapback UTE",
-    price: 89000,
-    images: ["https://images.unsplash.com/photo-1521369909029-2afed882baee?w=400&h=400&fit=crop"],
-    category: "Nón",
-    createdAt: Date.now(),
-    sold: 50,
-    views: 200,
-    discountPercent: 0,
-     stock: 20,
-  },
-  {
-    id: 4,
-    name: "Dây đeo thẻ UTE",
-    price: 45000,
-    images: ["https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?w=400&h=400&fit=crop"],
-    category: "Phụ kiện",
-    createdAt: Date.now(),
-    sold: 80,
-    views: 1000,
-    discountPercent: 10,
-     stock: 20,
-  },
-  {
-    id: 5,
-    name: "Áo Hoodie UTE",
-    price: 349000,
-    originalPrice: 399000,
-    images: ["https://images.unsplash.com/photo-1556821840-3a63f95609a7?w=400&h=400&fit=crop"],
-    category: "Áo",
-    isHot: true,
-    createdAt: Date.now(),
-    sold: 500,
-    views: 2000,
-    discountPercent: 15,
-     stock: 20,
-  },
-  {
-    id: 6,
-    name: "Túi đeo chéo UTE",
-    price: 159000,
-    images: ["https://images.unsplash.com/photo-1591561954557-26941169b49e?w=400&h=400&fit=crop"],
-    category: "Túi",
-    createdAt: Date.now(),
-    sold: 40,
-    views: 150,
-    discountPercent: 5,
-     stock: 20,
-  },
-  {
-    id: 7,
-    name: "Mũ Bucket UTE",
-    price: 69000,
-    images: ["https://images.unsplash.com/photo-1572307480813-ceb0e59d8325?w=400&h=400&fit=crop"],
-    category: "Nón",
-    createdAt: Date.now(),
-    sold: 250,
-    views: 1200,
-    discountPercent: 0,
-     stock: 20,
-  },
-  {
-    id: 8,
-    name: "Balo Mini UTE",
-    price: 199000,
-    images: ["https://images.unsplash.com/photo-1548036328-c9fa89d128fa?w=400&h=400&fit=crop"],
-    category: "Balo",
-    createdAt: Date.now(),
-    sold: 90,
-    views: 300,
-    discountPercent: 30,
-     stock: 20,
-  }
-];
+// const products: Product[] = [
+// {
+//     id: 1,
+//     name: "Balo UTE Premium",
+//     price: 299000,
+//     originalPrice: 399000,
+//     images: 
+//     [
+//       "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=400&h=400&fit=crop",
+//       "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400&h=400&fit=crop",
+//       "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?w=400&h=400&fit=crop"
+//     ],
+//     // image: "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=400&h=400&fit=crop",
+//     category: "Balo",
+//     isHot: true,
+//     createdAt: Date.now(),
+//     sold: 120,
+//     views: 500,
+//     discountPercent: 25,
+//     stock: 20
+//   },
+//   {
+//     id: 2,
+//     name: "Áo Polo UTE Classic",
+//     price: 189000,
+//     originalPrice: 249000,
+//     images: ["https://images.unsplash.com/photo-1581655353564-df123a1eb820?w=400&h=400&fit=crop"],
+//     category: "Áo",
+//     isNew: true,
+//     createdAt: Date.now(),
+//     sold: 300,
+//     views: 800,
+//     discountPercent: 20,
+//      stock: 20,
+//   },
+//   {
+//     id: 3,
+//     name: "Nón Snapback UTE",
+//     price: 89000,
+//     images: ["https://images.unsplash.com/photo-1521369909029-2afed882baee?w=400&h=400&fit=crop"],
+//     category: "Nón",
+//     createdAt: Date.now(),
+//     sold: 50,
+//     views: 200,
+//     discountPercent: 0,
+//      stock: 20,
+//   },
+//   {
+//     id: 4,
+//     name: "Dây đeo thẻ UTE",
+//     price: 45000,
+//     images: ["https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?w=400&h=400&fit=crop"],
+//     category: "Phụ kiện",
+//     createdAt: Date.now(),
+//     sold: 80,
+//     views: 1000,
+//     discountPercent: 10,
+//      stock: 20,
+//   },
+//   {
+//     id: 5,
+//     name: "Áo Hoodie UTE",
+//     price: 349000,
+//     originalPrice: 399000,
+//     images: ["https://images.unsplash.com/photo-1556821840-3a63f95609a7?w=400&h=400&fit=crop"],
+//     category: "Áo",
+//     isHot: true,
+//     createdAt: Date.now(),
+//     sold: 500,
+//     views: 2000,
+//     discountPercent: 15,
+//      stock: 20,
+//   },
+//   {
+//     id: 6,
+//     name: "Túi đeo chéo UTE",
+//     price: 159000,
+//     images: ["https://images.unsplash.com/photo-1591561954557-26941169b49e?w=400&h=400&fit=crop"],
+//     category: "Túi",
+//     createdAt: Date.now(),
+//     sold: 40,
+//     views: 150,
+//     discountPercent: 5,
+//      stock: 20,
+//   },
+//   {
+//     id: 7,
+//     name: "Mũ Bucket UTE",
+//     price: 69000,
+//     images: ["https://images.unsplash.com/photo-1572307480813-ceb0e59d8325?w=400&h=400&fit=crop"],
+//     category: "Nón",
+//     createdAt: Date.now(),
+//     sold: 250,
+//     views: 1200,
+//     discountPercent: 0,
+//      stock: 20,
+//   },
+//   {
+//     id: 8,
+//     name: "Balo Mini UTE",
+//     price: 199000,
+//     images: ["https://images.unsplash.com/photo-1548036328-c9fa89d128fa?w=400&h=400&fit=crop"],
+//     category: "Balo",
+//     createdAt: Date.now(),
+//     sold: 90,
+//     views: 300,
+//     discountPercent: 30,
+//      stock: 20,
+//   }
+// ];
 
 const categories = [
   { name: "Tất cả", value: "all" },
@@ -140,9 +140,10 @@ const categories = [
 export default function HomePage() {
   const [selectedCategory, setSelectedCategory] = useState("all");
   const [searchTerm, setSearchTerm] = useState("");
+  const [products, setProducts] = useState<Product[]>([]);
 
   const filteredProducts = products.filter(product => {
-    const matchesCategory = selectedCategory === "all" || product.category === selectedCategory;
+    const matchesCategory = selectedCategory === "all" || product.category.name === selectedCategory;
     const matchesSearch = product.name.toLowerCase().includes(searchTerm.toLowerCase());
     return matchesCategory && matchesSearch;
   });
@@ -153,6 +154,14 @@ export default function HomePage() {
       currency: 'VND'
     }).format(price);
   };
+
+  useEffect(() => {
+    const res = async () => {
+      const response = await productApi.getProducts();
+      setProducts(response.data || []);
+    }
+    res();
+  }, []);
 
   return (
     <div className="home-page">
